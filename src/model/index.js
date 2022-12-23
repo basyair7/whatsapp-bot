412b37f7bfd45237477c2e181afbb1a2f571a52a
Fathul <61180722+basyair7@users.noreply.github.com>
2022-12-24 02:27:46 +0700

    Update index.js

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,9 @@
 const getMessage = require("./dbMessage");
 const getNumber = require("./dbNumber");
+const DateTimeBot = require("./clock");
 
 module.exports = {
     getMessage,
     getNumber,
+    DateTimeBot,
 }
